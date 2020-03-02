0fbf06ffca997d34b2c4a9c92797e391e076ba4b
Iryna Lemeha <lemeha.iryna@gmail.com>
2020-03-02 07:10:48 -0800

    Add project files.

---
+++ ProductSKU/PageObject/ProductDetailsPage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductSKU.PageObject
+{
+    public class ProductDetailsPage
+    {
+        private IWebDriver _driver;
+
+        public ProductDetailsPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement VerifySKU => _driver.FindElement(By.XPath("//p[@class='sku']/span"));
+        private IWebElement CurrentPrice => _driver.FindElement(By.XPath("//span[@class='current_price']/span[@class='money']"));
+        private IList<IWebElement> ColorOptions => _driver.FindElements(By.CssSelector(".section.product_section.clearfix.js-product_section .swatch_options .color.available"));
+
+        public string GetProductDetailsSKUNumber()
+        {
+            return VerifySKU.Text;
+        }
+
+        public string GetProductDetailsCurrentPrice()
+        {
+            return CurrentPrice.Text;
+        }
+
+        public IList<IWebElement> GetColorOptions()
+        {
+            return ColorOptions;
+        }
+    }
+}
---
+++ ProductSKU/PageObject/ProductsPage.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductSKU.PageObject
+{
+    public class ProductsPage
+    {
+        private IWebDriver _driver;
+        public ProductsPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement InputSKUinFilter => _driver.FindElement(By.XPath("(//li[@class='search_container']//input[@name='q'])[2]"));
+        private IWebElement TitleSearch => _driver.FindElement(By.XPath("//h1[contains(.,'Search')]/parent::div"));
+        private IList<IWebElement> ProductTitle => _driver.FindElements(By.XPath("//div[@class='h5 sub_title']/a"));
+
+        //*[@id="shopify-section-product-template"]/div[1]/div[2]/div/div/div[1]/div[2]/p[2]/span[1]/span/span
+
+        /*public string VerifyPageOpened()
+        {
+            return PageTitleVerify.Text;
+        }    */
+        // Type Prduct SKU in Search filter field
+        public void SearchSKUNumber(string product)
+        {
+            InputSKUinFilter.SendKeys(product);
+
+            InputSKUinFilter.SendKeys(Keys.Enter);
+        }
+
+        public string VerifySearchTitle()
+        {
+            return TitleSearch.Text;
+        }
+
+        public int GetSearchResultProductsCount()
+        {
+            return ProductTitle.Count;
+        }
+
+        public void OpenFirstSearchResult()
+        {
+            ProductTitle.First().Click();
+        }
+    }
+
+}
